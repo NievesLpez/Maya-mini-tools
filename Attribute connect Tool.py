364ee5632c175f7790dd6fb20df5f1cfdc390d3e
NievesLpez <nieveslpez.p@gmail.com>
2025-05-03 18:21:09 +0200

    Add files via upload

---
+++ Attribute connect Tool.py
@@ -0,0 +1,51 @@
+"""
+Created by Nieves López (2025 May)
+
+"""
+
+import maya.cmds as cmds
+
+def connect_with_inputs():
+    source_input = cmds.promptDialog(
+        title="Attribute connector tool",
+        message="Enter name + . + attribute:",
+        button=["OK", "Cancel"],
+        defaultButton="OK",
+        cancelButton="Cancel",
+        dismissString="Cancel"
+    )
+    
+    if source_input == "Cancel":
+        return  
+    
+    source_plug = cmds.promptDialog(query=True, text=True)
+    
+    target_attr = cmds.promptDialog(
+        title="Target Attribute",
+        message="Enter target attribute:",
+        button=["OK", "Cancel"],
+        defaultButton="OK",
+        cancelButton="Cancel",
+        dismissString="Cancel",
+    )
+    
+    if target_attr == "Cancel":
+        return
+    
+    target_attr = cmds.promptDialog(query=True, text=True)
+    
+    selected_objects = cmds.ls(selection=True)  
+    
+    if not selected_objects:
+        cmds.warning("Invalid selection")
+        return
+    
+    for obj in selected_objects:
+        target_plug = f"{obj}.{target_attr}"
+        try:
+            cmds.connectAttr(source_plug, target_plug, force=True)
+            print(f"Connected: {source_plug} → {target_plug}")
+        except Exception as e:
+            cmds.warning(f" Failed to connect {source_plug} to {target_plug}: {str(e)}")
+
+connect_with_inputs()
