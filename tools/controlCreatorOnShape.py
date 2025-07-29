599a404e700992f09d5a98f85632df342582782a
Nieves Yashuang López P. <nieveslpez.p@gmail.com>
2025-07-30 00:45:07 +0200

    Update controlCreatorOnShape.py

--- tools/controlCreatorOnShape.py
+++ tools/controlCreatorOnShape.py
@@ -3,7 +3,7 @@ Author: Nieves López
 
 May 2025
 
-Description : For selected NURBS shapes creates the main CTL structure and adds ?CTL suffix. Just name your desired curved and done.
+Description : For selected NURBS shapes, creates the main control structure and adds a CTL suffix. Simply name your desired curve and you're done
 """
 import maya.cmds as cmds
 
